96ada822480d8a8dd596de93aac4805d41fd6285
Nupur Gupta <nupur2000gupta@gmail.com>
2021-06-07 17:14:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@ import 'firebase/firestore'
 import "firebase/auth"
 
 var firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBjmHeRajO2FGJ48xsNUvbaibRyNUp1cB4",
-  authDomain: "temp-b2e0d.firebaseapp.com",
-  databaseURL: "https://temp-b2e0d-default-rtdb.firebaseio.com",
-  projectId: "temp-b2e0d",
-  storageBucket: "temp-b2e0d.appspot.com",
-  messagingSenderId: "327849470486",
-  appId: "1:327849470486:web:dff061207d43d00ebb6337",
-  measurementId: "G-8YMTB8EQ7P"
+  
 })
 var db = firebaseApp.firestore()
 export const auth = firebaseApp.auth()
